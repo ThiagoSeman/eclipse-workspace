a0c09b76f4da57e5d54ea0f53bc306bce1d7470f
ThiagoSeman <71970502+ThiagoSeman@users.noreply.github.com>
2020-10-03 17:22:33 -0300

    Initial commit

---
+++ teste publica/src/teste_publica/dados.java
@@ -0,0 +1,23 @@
+package teste_publica;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+// array que guarda os dados que são digitado
+
+
+public class dados {
+	
+	List<jogos> dados = new ArrayList<>();
+
+	public List<jogos> getDados() {
+		return dados;
+	}
+
+	public void setDados(List<jogos> dados) {
+		this.dados = dados;
+	}
+	
+
+}
